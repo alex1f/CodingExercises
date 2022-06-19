386abe15878ba02759d880df93c18d5592de08da
alex1f <60682504+alex1f@users.noreply.github.com>
2022-06-19 21:12:11 +0300

    Refactor to move reading the input into a separate method to make the code more pretty

--- Yandex/AlgoTraining/src/main/java/HW4_A_Deliveries.java
+++ Yandex/AlgoTraining/src/main/java/HW4_A_Deliveries.java
@@ -4,33 +4,37 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class HW4_A_Deliveries {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Box> boxes = readBoxData();
+        Map<Long, Long> frequencies = calculateColorFrequencies(boxes);
+        frequencies.forEach((key, value) -> System.out.println(key + " " + value));
+    }
 
-        int totalDeliveries = Integer.parseInt(reader.readLine());
-
+    private static List<Box> readBoxData() throws IOException{
         List<Box> boxes = new ArrayList<>();
 
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int totalDeliveries = Integer.parseInt(reader.readLine());
         for (int i = 0; i < totalDeliveries; i++){
             String input = reader.readLine();
             String[] rawNumbers = input.split(" ");
             long colorCode = Long.parseLong(rawNumbers[0]);
             long colorValue = Long.parseLong(rawNumbers[1]);
-
             boxes.add(new Box(colorCode, colorValue));
-
         }
-
-        Map<Long, Long> frequencies = calculateColorFrequencies(boxes);
-        frequencies.forEach((key, value) -> System.out.println(key + " " + value));
+        return boxes;
     }
 
     public static Map<Long, Long> calculateColorFrequencies (List<Box> boxes){
-        return boxes.stream().collect(Collectors.groupingBy(Box::getColorCode, Collectors.summingLong(Box::getColorValue)));
-
+        return boxes.stream()
+                .collect(Collectors.groupingBy(
+                        Box::getColorCode,
+                        TreeMap::new,
+                        Collectors.summingLong(Box::getColorValue))
+                );
     }
 }
