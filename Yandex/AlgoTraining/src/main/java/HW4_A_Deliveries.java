c62d3d04e6f758c80119cdd6700e532c781e8fb5
alex1f <60682504+alex1f@users.noreply.github.com>
2022-06-19 21:20:39 +0300

    Return reading input into main method, because otherwise the time limit for the solution was exceeded. So no pretty code here :(

--- Yandex/AlgoTraining/src/main/java/HW4_A_Deliveries.java
+++ Yandex/AlgoTraining/src/main/java/HW4_A_Deliveries.java
@@ -8,21 +8,16 @@ import java.util.stream.Collectors;
 
 public class HW4_A_Deliveries {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        int totalDeliveries = Integer.parseInt(reader.readLine());
-
         List<Box> boxes = new ArrayList<>();
 
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int totalDeliveries = Integer.parseInt(reader.readLine());
         for (int i = 0; i < totalDeliveries; i++){
             String input = reader.readLine();
             String[] rawNumbers = input.split(" ");
             long colorCode = Long.parseLong(rawNumbers[0]);
             long colorValue = Long.parseLong(rawNumbers[1]);
-
             boxes.add(new Box(colorCode, colorValue));
-
         }
 
         Map<Long, Long> frequencies = calculateColorFrequencies(boxes);
